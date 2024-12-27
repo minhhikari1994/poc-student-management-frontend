c1e19bf38d4b59b092ce404e68a4d1498c41b72e
Minh Hikari <minhhikari1994+dev@outlook.com>
2024-12-27 22:50:48 +0700

    fix startup + add credentials

--- services/authService.js
+++ services/authService.js
@@ -1,14 +1,16 @@
-const baseApiUrl = 'http://localhost:5000'
+const baseApiUrl = 'http://192.168.1.12:5000'
 
 export const login = (email, password) => {
     return $fetch(`${baseApiUrl}/api/login`, {
        method: 'POST',
-       body: { login_id: email, password } 
+       body: { login_id: email, password },
+       credentials: 'include'
     });
 }
 
 export const logout = () => {
     return $fetch(`${baseApiUrl}/api/logout`, {
-       method: 'POST'
+       method: 'POST',
+       credentials: 'include'
     });
 }
