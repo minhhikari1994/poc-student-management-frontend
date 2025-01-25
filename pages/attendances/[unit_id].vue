bf056bd2eb85df207a355bf556c008c8c37c046d
Minh Hikari <minhhikari1994+dev@outlook.com>
2025-01-25 12:44:55 +0700

    update style of the carousel

--- pages/attendances/[unit_id].vue
+++ pages/attendances/[unit_id].vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex w-full items-center justify-center">
-        <UiDatepicker v-model="attendanceDate">
+        <UiDatepicker v-model="attendanceDate" @update:model-value="onAttendanceDateChange">
             <template #default="{ togglePopover }">
                 <UiButton variant="outline"
                     :class="[!attendanceDate && 'text-muted-foreground', 'w-full justify-start text-left']"
@@ -21,20 +21,17 @@
         </UiSelect>
     </div>
 
-
-
-    <div class="flex items-center justify-center mt-4" v-if="attendanceDate && selectedAttendanceType">
-        <UiCarousel class="relative w-full max-w-sm" :opts="{ align: 'start' }">
-            <UiCarouselContent>
+    <div class="flex items-center justify-center mt-[4rem]" v-if="attendanceDate && selectedAttendanceType">
+        <UiCarousel class="max-w-xsw-full relative w-full max-w-xs" :opts="{ align: 'center' }" orientation="vertical">
+            <UiCarouselContent class="-mt-1 h-[350px]">
                 <UiCarouselItem v-for="student in unitDetails.students" :key="student.student_code" grab-cursor
-                    class="md:basis-1/2 lg:basis-1/2">
+                    class="md:basis-1/2">
                     <div class="p-1">
                         <UiCard>
-                            <UiCardContent
-                                class="flex flex-col aspect-square items-center justify-center text-center p-6">
+                            <UiCardContent class="flex flex-col aspect-square items-center justify-center text-center">
                                 <span class="text-xl font-semibold">{{ getStudentFullName(student) }}</span>
                                 <span class="text-muted-foreground">{{ student.student_code }}</span>
-                                <div class="flex w-full justify-center mt-20">
+                                <div class="flex w-8/12 justify-center mt-20">
                                     <UiRadioGroup default-value="default">
                                         <div class="flex items-center space-x-2">
                                             <UiRadioGroupItem id="r1" value="default" />
@@ -59,33 +56,11 @@
             <UiCarouselNext />
         </UiCarousel>
     </div>
-    <!-- <div class="flex items-center justify-center">
-      <UiList class="">
-        <template v-for="student in unitDetails.students" :key="student.student_code">
-          <UiListItem class="py-5 items-start px-0">
-            <UiListContent>
-              <UiListTitle :title="`${student.saint_name} ${student.last_name} ${student.middle_name} ${student.first_name}`" />
-              <UiListSubtitle
-                class="line-clamp-2"
-                :subtitle="student.student_code"
-              />
-            </UiListContent>
-            <UiButton
-              size="icon-sm"
-              variant="ghost"
-              class="ml-auto shrink-0 self-center rounded-full"
-            >
-              <Icon name="lucide:chevron-right" />
-            </UiButton>
-          </UiListItem>
-          <UiSeparator class="ml-auto last:hidden" />
-        </template>
-      </UiList>
-    </div> -->
 </template>
 
 <script lang="js" setup>
-import { format } from "date-fns";
+import { format , isSunday} from "date-fns";
+import { push } from "notivue";
 
 import { useUnitStore } from "@/stores/unitStore";
 import { useAppPageStore } from "@/stores/appPageStore";
@@ -119,6 +94,14 @@ onBeforeMount(async () => {
 
 const getStudentFullName = (student) => {
     return `${student.saint_name} ${student.last_name} ${student.middle_name} ${student.first_name}`;
+}
+
+const onAttendanceDateChange = (date) => {
+    console.log(date);
+    if (!isSunday(date)) {
+        push.error("Ngày bạn đang chọn không phải là Chúa Nhật")
+        attendanceDate.value = null
+    }
 }
 
 </script>
